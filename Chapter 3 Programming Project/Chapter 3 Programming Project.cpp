183191d9701b4e55aab00e5000e56973bd4b4cd9
Jesse zheng <BessiieBoi@outlook.com>
2025-02-25 00:26:04 -0500

    Version1

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -1,2 +1,18 @@
-//Chapter 3 Programming Project
-//MAIN
+//Chapter 3 Programming Project - Math tutor
+#include <iostream>
+
+int main() {
+	//Displays math problem
+	std::cout
+		<< " 247\n"
+		<< "\033[4m+129\033[0m\n" //Use \033[4m to start an underline and \033[0m to end an underline
+		<< "Enter your answer, or press any key then Enter to see the solution: ";
+	//recognizes there is an input to continue
+	std::cin.get();
+	//Outputs Solution
+	std::cout 
+		<< " 247\n" 
+		<< "\033[4m+129\033[0m\n" 
+		<< " 376";
+	return 0;
+}
