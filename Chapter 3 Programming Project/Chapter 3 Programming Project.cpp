8f3ea7e67e965c8b3492db51a45f4c6deedf71c6
Jesse zheng <BessiieBoi@outlook.com>
2025-02-25 23:19:11 -0500

    Adjusted

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -1,10 +1,4 @@
 //Chapter 3 Programming Project - Ingredient Adjuster
-//A cookie recipe calls for the following ingredients:
-//1.5 cups of sugar
-//1 cup of butter
-//2.75 cups of flour
-//The recipe produces 48 cookies with this amount of ingredients. 
-//Write a program that asks the user how many cookies he or she wants to make, then displays the number of cups of each ingredient needed for the specified number of cookies.
 
 #include <iostream>
 #include <iomanip>
@@ -16,14 +10,18 @@ const double amountFlourRatio = 0.0572916;
 
 
 int main() {
+
 	float amountCookies;
+
 	//Display question and recieve inputs
 	std::cout 
 		<< "How many cookies do you want to make?" << std::endl;
 	std::cin
 		>> amountCookies;
+
 	//Rounds by 2 places
 	std::cout << std::fixed << std::setprecision(2);
+
 	//Output
 	std::cout
 		<< "You will need:\n"
