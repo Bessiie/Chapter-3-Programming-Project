414bc67d00b71425a18eaf7f65bbb3de96fef0ef
Jesse zheng <BessiieBoi@outlook.com>
2025-02-25 23:19:42 -0500

    Adjusted

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 
 int main() {
+
 	//Displays math problem
 	std::cout
 		<< " 247\n"
 		<< "\033[4m+129\033[0m\n" //Use \033[4m to start an underline and \033[0m to end an underline
 		<< "Enter your answer, or press any key then Enter to see the solution: ";
+
 	//recognizes there is an input to continue
 	std::cin.get();
+
 	//Outputs Solution
 	std::cout 
 		<< " 247\n" 
