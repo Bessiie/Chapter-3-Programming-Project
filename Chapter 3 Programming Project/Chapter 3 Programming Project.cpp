480f30ad28baeed54fb529873f7edbdeb03605ac
Jesse zheng <BessiieBoi@outlook.com>
2025-03-03 11:16:36 -0500

    Added Comments

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -1,15 +1,16 @@
 //Chapter 3 Programming Project - Math tutor
 #include <iostream>
 
+//Main Function
 int main() {
 
 	//Displays math problem
 	std::cout
 		<< " 247\n"
-		<< "\033[4m+129\033[0m\n" //Use \033[4m to start an underline and \033[0m to end an underline
+		<< "\033[4m+129\033[0m\n" //Used \033[4m to start an underline and \033[0m to end an underline
 		<< "Enter your answer, or press any key then Enter to see the solution: ";
 
-	//recognizes there is an input to continue
+	//recognizes there is an input and continues
 	std::cin.get();
 
 	//Outputs Solution
