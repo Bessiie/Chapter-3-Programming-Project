405ecffbe2e10d7e4d9a6b197006c72b67b1121b
Jesse zheng <BessiieBoi@outlook.com>
2025-03-03 11:47:45 -0500

    Changed output message

--- Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project/Chapter 3 Programming Project.cpp
@@ -8,7 +8,8 @@ int main() {
 	std::cout
 		<< " 247\n"
 		<< "\033[4m+129\033[0m\n" //Used \033[4m to start an underline and \033[0m to end an underline
-		<< "Enter your answer, or press any key then Enter to see the solution: ";
+		<< "Press enter to reveal solution" 
+		<< std::endl;
 
 	//recognizes there is an input and continues
 	std::cin.get();
